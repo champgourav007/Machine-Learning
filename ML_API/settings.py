2224fcc53062248c08462d63baa016137615ac02
Gourav Kumar Singh <gouravrajput257@gmail.com>
2022-11-03 08:06:00 -0500

    Check for env

--- ML_API/settings.py
+++ ML_API/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
+
+print(os.environ)
 
 # setting up environment object
 env = environ.Env()
