43c68d564da044382c0e082b757a82db8e9b12c2
Gourav Kumar Singh <gouravrajput257@gmail.com>
2022-11-03 07:59:56 -0500

    changes for ci

--- ML_API/settings.py
+++ ML_API/settings.py
@@ -16,7 +16,7 @@ import environ
 # setting up environment object
 env = environ.Env()
 environ.Env.read_env()
-
+print(env('DJANGO_SECRET_KEY'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
